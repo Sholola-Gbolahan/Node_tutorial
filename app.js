a871e6976881e3caf785a81d67ca4d848a512278
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-29 16:10:41 +0100

    1. introduction section.

--- app.js
+++
@@ -1,8 +0,0 @@
-const amount = 9
-
-if (amount < 10) {
-  console.log("small amount")
-} else {
-  console.log("big amount")
-}
-console.log("Yeeeppp")
---
+++ intro.js
@@ -0,0 +1,8 @@
+const amount = 9
+
+if (amount < 10) {
+  console.log("small amount")
+} else {
+  console.log("big amount")
+}
+console.log("Yeeeppp")
