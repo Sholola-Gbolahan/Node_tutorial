64f8e06c344a2a41bf64dfc91b31ec9016f7a6f2
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-29 16:30:20 +0100

    2. Global

---
+++ app.js
@@ -0,0 +1,14 @@
+// GLOABALS   - NO WINDOW
+
+//  __dirname   - path to current directory
+// __filename   - filename
+// require   -  function to use modules (CommonJs)
+// module  - info about current module (file)
+// process   - info about env where the program is being executed
+
+console.log(__dirname)
+console.log(
+  setInterval(() => {
+    console.log("hello Wolrd")
+  }, 1000)
+)
