1a09cd3c18a84f8c888ac3d463f67354fb713c52
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-30 15:45:45 +0100

    4. running a function directly without the export

--- app.js
+++ app.js
@@ -4,10 +4,4 @@
 const names = require("./name")
 const sayHi = require("./utils")
 
-const data = require("./alternative-flavour")
-
-sayHi("susan")
-sayHi(names.peter)
-sayHi(names.john)
-
-console.log(data)
+require("./mind-grenade")
