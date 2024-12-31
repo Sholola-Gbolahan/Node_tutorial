9b526d8b4a3be8d61580f4787dd4f0a6a45030aa
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-12-31 08:46:12 +0100

    12.a Http more feature

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // getting the http module
-const http = require('http')
+const http = require("http")
 
 // creating server from the http
-const server = http.createServer((req,res)=>{
-res.write('Welcome to my homepage')
-res.end()
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to home page")
+    return
+  }
 
+  if (req.url === "/about") {
+    res.end("Here is our short history")
+    return
+  }
+
+  res.end(`
+    <h1>Oops</h1>
+    <p>We can't seem to find the page you're looking for </p>
+    <a href='/'> Home</a>
+    `)
 })
 
 // creating port to listen to server
