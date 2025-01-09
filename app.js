ba7a65e3ee53fcdfeef22566dd0eff95cf24ea17
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-09 11:07:00 +0100

    16.1 Node native option 1

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const { readFile } = require("fs")
-const { result } = require("lodash")
+const { readFile, writeFile } = require("fs")
+const util = require('util')
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async () =>{
   try {
-    const first = await getText('./content/first.txt')
-    const second = await getText('./content/second.txt')
+    const first = await readFilePromise('./content/first.txt','utf8')
+    const second = await readFilePromise('./content/second.txt','utf8')
+
+    await writeFilePromise('./content/result-granade.txt',`Hellooo : ${first} ---- ${second}`)
+
     console.log(first)
     console.log(second)
+
   }
 
   catch(error) {
