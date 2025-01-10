974d81f425c2dd69af193410146a4e8a5558cb4d
Sholola-Gbolahan <shololagbolahan@gmail.com>
2025-01-10 10:34:03 +0100

    17.2 Request event usinf HTTP

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const customEmitter =new EventsEmitter();
 
 // On - listen for an event
 // emit - Emit/ carry out an event
-customEmitter.on('response', ()=> {
-  console.log('data received');
+customEmitter.on('response', (name, age)=> {
+  console.log(`data received: user info, name: ${name}, age:${age}`);
   
 })
 
-customEmitter.emit('response')
+customEmitter.on('response', ()=> { 
+  console.log(`Some data are received`);
+  
+})
+
+customEmitter.emit('response','john', 31)
